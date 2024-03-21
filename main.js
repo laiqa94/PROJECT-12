9fc63898a8004c08d8c2cb058f29a483af921d2e
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 01:52:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['laiqa', 'maryam', 'zainab', 'sana'];
+const message = 'Wellcome to Typescript class:';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ main.ts
@@ -0,0 +1,10 @@
+
+let names :string[] =['laiqa','maryam','zainab','sana'];
+
+const message :string = 'Wellcome to Typescript class:'
+
+for(let i=0; i<names.length; i++){
+
+console.log(message + names[i]);
+
+}
